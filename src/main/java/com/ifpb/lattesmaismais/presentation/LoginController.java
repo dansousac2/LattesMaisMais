6cadb27b1d90e60bde711e14f5d695a14ddb1933
DanSousa <dansousac2@gmail.com>
2023-04-09 19:33:25 -0300

    successfully login with JDBC credentials

--- src/main/java/com/ifpb/lattesmaismais/presentation/LoginController.java
+++ src/main/java/com/ifpb/lattesmaismais/presentation/LoginController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +24,7 @@ public class LoginController {
 	@Autowired
 	private TokenService tokenService;
 	
+	@PostMapping
 	public String loginLocal(@RequestBody LoginDto loginDto) {
 		
 		UsernamePasswordAuthenticationToken usernamePAToken = 
--- src/main/java/com/ifpb/lattesmaismais/security/SecurityConfig.java
+++ src/main/java/com/ifpb/lattesmaismais/security/SecurityConfig.java
@@ -2,9 +2,15 @@ package com.ifpb.lattesmaismais.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.ifpb.lattesmaismais.business.interfaces.RoleService;
@@ -18,15 +24,27 @@ public class SecurityConfig {
 		
 		http
 			.csrf().disable()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
 			.authorizeHttpRequests(auth -> {
 				auth.requestMatchers("/api/curriculum").permitAll();
 				auth.requestMatchers("/logout").permitAll();
+				auth.requestMatchers(HttpMethod.POST, "/api/login").permitAll();
 				auth.requestMatchers("/api/curriculum/validator").hasRole(RoleService.AVALIABLE_ROLES.VALIDATOR.name());
 				auth.anyRequest().authenticated();
-			})
-			.formLogin(Customizer.withDefaults());
+			});
+//			.formLogin(Customizer.withDefaults());
 			
-		
 		return http.build();
+	}
+	
+	@Bean
+	AuthenticationManager authenticationManager(AuthenticationConfiguration authConf) throws Exception{
+		return authConf.getAuthenticationManager();
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
